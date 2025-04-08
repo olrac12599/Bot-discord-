dbef890139be57218798c1e4c1c1ad50845846ef
olrac12599 <charlestrabucchi@gmail.com>
2025-04-08 06:24:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -104,10 +104,13 @@ async def update_stream_notifications():
         text_channel = bot.get_channel(TEXT_NOTIFY_CHANNEL_ID)
 
         # Notif @everyone pour nouveaux streamers en live
-new_live = live_now - already_live
-for new_streamer in new_live:
-    msg = await text_channel.send(f"🚨 **{new_streamer} est en live !** @everyone\nhttps://twitch.tv/{new_streamer}")
-    await msg.delete(delay=5)  # Supprime le message après 5 secondes
+        new_live = live_now - already_live
+        for new_streamer in new_live:
+            msg = await text_channel.send(f"🚨 **{new_streamer} est en live !** @everyone\nhttps://twitch.tv/{new_streamer}")
+            await msg.delete(delay=5)  # Supprime le message après 5 secondes
+
+        # Met à jour les streamers déjà en live
+        already_live = live_now
 
         if live_now:
             embed = discord.Embed(
