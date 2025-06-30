23795ab21688c4981ee41fe082e4398ff987d278
olrac12599 <charlestrabucchi@gmail.com>
2025-06-30 21:14:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,11 +32,11 @@ def record_game(url, duration=10):
         chromedriver_autoinstaller.install()
 
         chrome_options = Options()
-        # chrome_options.add_argument("--headless")  # désactive pour tester
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--window-size=1280,720")
-
+# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--window-size=1280,720")
+chrome_options.add_argument(f"--user-data-dir=/tmp/selenium_profile_{time.time()}")  # 👈 ajoute cette ligne
         driver = webdriver.Chrome(options=chrome_options)
         print("[DEBUG] Navigateur lancé")
         driver.get(url)
