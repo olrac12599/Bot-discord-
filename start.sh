427328cc66633e0d36de395adcc943bec5286b55
olrac12599 <charlestrabucchi@gmail.com>
2025-07-03 05:30:03 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# 1. Lancer l'affichage virtuel pour le navigateur
-Xvfb :0 -screen 0 1280x720x24 &
-
-# 2. Lancer le bot Discord
-python3 main.py
+# Lance le serveur Xvfb et exécute le bot à l'intérieur.
+# Ceci garantit que l'affichage est prêt avant que le bot ne démarre.
+xvfb-run python3 main.py
