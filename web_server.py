b2fa0107eb5a33f6d67d22bee13e1f4ebe0b5f67
olrac12599 <charlestrabucchi@gmail.com>
2025-07-03 04:02:32 +0200

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -1,1 +1,18 @@
-d
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return "✅ Serveur Flask actif. Va sur /live"
+
+@app.route("/live")
+def serve_novnc():
+    return send_from_directory("novnc", "vnc.html")
+
+@app.route("/<path:path>")
+def serve_files(path):
+    return send_from_directory("novnc", path)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
