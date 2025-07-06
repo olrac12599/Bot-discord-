7d99fbdb4bea55ec3b160adfddfd58d6b15dce8c
olrac12599 <charlestrabucchi@gmail.com>
2025-07-06 18:03:30 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,1 @@
-#!/bin/sh
 
-# Démarrer le serveur d'affichage virtuel Xvfb en arrière-plan
-# sur l'écran :99 avec une résolution de 1280x720 et une profondeur de couleur de 16 bits.
-Xvfb :99 -screen 0 1280x720x16 &
-
-# Laisser un court instant au serveur Xvfb pour se lancer complètement
-sleep 2
-
-# Exécuter la commande principale de l'application (votre bot Python)
-exec python main.py
