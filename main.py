cd478d3f153adaa24aff10f4a402a8db7f6b103b
olrac12599 <charlestrabucchi@gmail.com>
2025-07-06 08:51:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-
 import subprocess
 
-path = subprocess.getoutput('which stockfish')
-print(f"Stockfish path: {path}")
+# Vérifier où Stockfish est installé
+stockfish_path = subprocess.getoutput('which stockfish')
+if stockfish_path:
+    print(f"Stockfish est installé dans : {stockfish_path}")
+else:
+    print("Stockfish n'est pas installé ou introuvable dans le PATH.")
