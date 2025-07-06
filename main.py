cc87aff4ac7f8dd3291ace7fdd408d3f09617a90
olrac12599 <charlestrabucchi@gmail.com>
2025-07-06 08:54:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import subprocess
 
-# Vérifier où Stockfish est installé
-stockfish_path = subprocess.getoutput('which stockfish')
-if stockfish_path:
-    print(f"Stockfish est installé dans : {stockfish_path}")
-else:
-    print("Stockfish n'est pas installé ou introuvable dans le PATH.")
+# Lister les fichiers installés dans /usr/bin
+files = subprocess.getoutput('ls /usr/bin')
+print(f"Fichiers dans /usr/bin : {files}")
